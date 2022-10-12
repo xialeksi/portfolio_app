4423d90a27a588eaf7b9414ebc47a2b0af08823d
Aleksip <xial.aleksi@gmail.com>
2022-10-12 09:40:15 +0300

    base Project and Image classes

---
+++ Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioApp
+{
+    internal class Image
+    {
+        public int idimage { get; set; }
+        public int idproject { get; set; }
+        public string filename { get; set; }
+        public string desc { get; set; }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioApp
+{
+    public class Project
+    {
+        public int idproject { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public Project(int id, string n, string d)
+        {
+            idproject = id;
+            name = n;
+            description = d;
+        }
+    }
+}
